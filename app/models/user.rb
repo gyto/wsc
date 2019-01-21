c392bf98692c6a2ddf641b1a32973720dfb8a18d
Roman Kovtun <gyto23@gmail.com>
2019-01-20 20:28:34 -0600

    Implement Account design

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
 
   has_many :orders
   has_one :account
+
+  before_create :set_user_role_by_default
+
+  def set_user_role_by_default
+    self.roles << Role.find_by_name('customer') if self.role_ids.count.zero?
+  end
 end
